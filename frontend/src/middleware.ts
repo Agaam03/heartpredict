549bed37f668ad16544c77de67067878ef10bc00
Agaam03 <agam.prasetya03@gmail.com>
2025-09-01 18:55:36 +0700

    refactor: improve middleware structure and readability by organizing authentication checks

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -5,33 +5,43 @@ import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, predictRoutes, publi
 
 export const { auth } = NextAuth(authConfig)
 export default auth((req: NextRequest & { auth?: any }) => {
-    const { nextUrl } = req;
-    const isLoggedIn = !!req.auth;
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    if (isApiAuthRoute) return NextResponse.next();
+  // ✅ Deteksi crawler bot
+  const ua = req.headers.get("user-agent") || "";
+  const isBot = /facebookexternalhit|twitterbot|linkedinbot|googlebot/i.test(ua);
 
-    if (isAuthRoute) {
-        if (isLoggedIn) {
-            return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-        }
-        return NextResponse.next();
+  if (isApiAuthRoute) return NextResponse.next();
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-
-    if (!isLoggedIn && !isPublicRoute) {
-        let callbackUrl = nextUrl.pathname;
-        if (nextUrl.search) {
-            callbackUrl += nextUrl.search;
-        }
-        const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-        return NextResponse.redirect(new URL(`/login?callbackUrl=${encodedCallbackUrl}`, nextUrl));
-    }
-
     return NextResponse.next();
-})
+  }
+
+  // ✅ izinkan bot mengakses tanpa login
+  if (isBot) {
+    return NextResponse.next();
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+    return NextResponse.redirect(new URL(`/login?callbackUrl=${encodedCallbackUrl}`, nextUrl));
+  }
+
+  return NextResponse.next();
+});
+
 export const config = {
     matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
