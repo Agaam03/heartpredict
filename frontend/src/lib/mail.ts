da60d9d5e8ff5991d95e648577de2455a57e7b3f
Agaam03 <agam.prasetya03@gmail.com>
2025-10-01 18:53:41 +0700

    fix email

--- frontend/src/lib/mail.ts
+++ frontend/src/lib/mail.ts
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 
     await resend.emails.send({
-        from: "underworld-company@resend.dev",
+        from: "agamdeveloper.dev@company",
         to: email,
         subject: "2FA code",
         html: `
@@ -17,7 +17,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
     const resetLink = `${process.env.NEXT_PUBLIC_APP_URL}/new-password?token=${token}`;
 
     await resend.emails.send({
-        from: "underworld-company@resend.dev",
+        from: "agamdeveloper.dev@company",
         to: email,
         subject: "Reset your password",
         html: `
@@ -30,7 +30,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     const verificationUrl = `${process.env.NEXT_PUBLIC_APP_URL}/new-verification?token=${token}`;
 
     await resend.emails.send({
-        from: "underworld-company@resend.dev",
+        from: "agamdeveloper.dev@company",
         to: email,
         subject: "Verify your email",
         html: `
